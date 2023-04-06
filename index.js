4232367d95163763142b23f682e716cc97a16d92
Mariano <ma.marianoalvarez@gmail.com>
2023-04-06 15:31:32 +0200

    Termiando proyecto

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const puppeteer = require('puppeteer');
     await page.keyboard.press('Enter');
 
     await page.waitForSelector('#contents > ytd-video-renderer:nth-child(1) img');
+    await page.screenshot({path: './src/screenshot/youtube.jpg'});
     await page.click('#contents > ytd-video-renderer:nth-child(1) img');
     
     await page.waitForNavigation(); 
@@ -42,5 +43,45 @@ const puppeteer = require('puppeteer');
     }
 )();
 
-//
+// Ejecuta una busqueda en Media Markt y hace captura de los 10 primeros productos de la busqueda "Playstation 5".
+(async () => {
+    const browser = await puppeteer.launch( { headless: false, slowMo: 50 });
+    // const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://www.mediamarkt.es/');
+    await page.setViewport({width: 1080, height: 1024});
+
+    await page.waitForSelector('button#pwa-consent-layer-accept-all-button');   
+    await page.keyboard.press('Enter');
+    await page.screenshot({path: './src/screenshot/mediamarkt.jpg'});
+
+    await page.waitForSelector('input.SearchForm__StyledTextInput-sc-2778fe1b-1');
+    await page.type('input.SearchForm__StyledTextInput-sc-2778fe1b-1', 'Playstation 5');
+    await page.keyboard.press('Enter');
+
+    await page.waitForTimeout(2000);
+    await page.screenshot({path: './src/screenshot/mediamarkt-productos.jpg'});
+
+    const enlaces = await page.evaluate(() => {
+        const products = document.querySelectorAll('a[data-test="mms-product-list-item-link"]');
+        const titles = [];
+        for (let i = 0; i < 3; i++) {
+            titles.push(products[i].href);
+        }
+        return titles;
+    });
     
+    for (let i = 0; i < enlaces.length; i++) {
+        await page.goto(enlaces[i]);
+        await page.waitForSelector('h1.sc-hLBbgP');
+        await page.evaluate(() => {
+            window.scrollBy(0, 100);
+        });
+        await page.waitForTimeout(2000);
+        await page.screenshot({path: `./src/screenshot/mediamarkt-producto-${i}.jpg`});
+    }
+
+    await browser.close();
+    }
+)();
